cdbee317c294a36619b7dc1474f8b87d68614b5c
Rongzheng He <rongzhenghe@MacBook-Pro-9945.local>
2021-11-29 16:44:34 -0500

    first commit

---
+++ javaDocProject/src/code/JavaDocExample.java
@@ -0,0 +1,26 @@
+package code;
+
+/**
+ * This is my first JavaDoc
+ *
+ * @author he, rongzheng
+ */
+public class JavaDocExample {
+    /**
+     * This is the main function of JavaDocExample class
+     * @param args
+     */
+    public static void main(String[] args) {
+        JavaDocExample.des();
+        }
+    /**
+     * This print out something to the console
+     *
+     * @param There is no parameter for this des() method
+     * @return It will print out "This is a JavaDoc practice." to the console
+     */
+    public static void des() {
+        System.out.println("This is a JavaDoc practice example.");
+    }
+
+}
